6a4d587857b2914bcca9da4a4e8f6bc09069571a
Antonderk <99094675+Antonderk@users.noreply.github.com>
2022-02-12 22:23:15 -0500

    Add files via upload

--- src/project1/Location.java
+++ src/project1/Location.java
@@ -1,5 +1,12 @@
 package project1;
 
+/**
+ * This class contains a fixed set of possible locations with the instance attributes consisting of zipCode and
+ * cityName and methods that retrieve these attributes and compare Location objects.
+ *
+ * @author Darrel Dsouza
+ * @author Anton Derkach
+ */
 public enum Location {
 	SOMERSET 	("08807", "Bridgewater"), 
 	MIDDLESEX 	("08854", "Piscataway"),
@@ -9,37 +16,66 @@ public enum Location {
 	
 	private final String zipCode;
 	private final String cityName;
-	
+
+	/**
+	 * Instantiates a Location object
+	 * @param zipCode The String containing the zipcode
+	 * @param cityName The String containing the city name
+	 */
 	Location(String zipCode, String cityName) {
 		this.zipCode = zipCode;
 		this.cityName = cityName;
 	}
-	
+
+	/**
+	 * Compares this Location object with another Location object to check if they have the county name.
+	 * @param loc The location object to compare with.
+	 * @return True if the locations have the same county name.
+	 */
 	public boolean equals(Location loc) {
-		if(this.name().equals(loc.name())) {
+		if (this.name().equals(loc.name())) {
 			return true;
 		}
 		return false;
 	}
 
+	/**
+	 * Accesses the zipcode of the instance object.
+	 * @return The String representation of the zipcode.
+	 */
 	public String getZipCode() {
 		return zipCode;
 	}
-	
+
+	/**
+	 * Converts the location into a String consisting of the city name, zipcode, and county name.
+	 * @return The String representation of the location object consisting of the city name, zipcode, and county name.
+	 */
 	@Override
 	public String toString() {
 		return cityName + " " + zipCode + ", " + this.name();
 	}
-	
+
+	/**
+	 * Uses the String location input to access the enum location object with the input county name
+	 * if the input name exists.
+	 * Returns null if the given county name is not in the set of predefined county names.
+	 * @param location The String county name
+	 * @return The enum Location  object.
+	 */
 	public static Location getEnumFromString(String location) {
-		for(Location loc: Location.values()) {
-			if(loc.name().equals(location.toUpperCase())) {
+		for (Location loc: Location.values()) {
+			if (loc.name().equals(location.toUpperCase())) {
 				return loc;
 			}
 		}
 		return null;
 	}
-	
+
+	/**
+	 * Tests the Location class.
+	 * @param args Not used. Here by default.
+	 */
 	public static void main(String[] args) {
 		Location location1 = Location.SOMERSET;
 		System.out.println(location1.toString());
--- src/project1/Patient.java
+++ src/project1/Patient.java
@@ -1,46 +1,76 @@
 package project1;
 
+/**
+ * This class contains a constructor that instantiates a Patient object with the attributes consisting of
+ * the first name, last name, and DOB and the methods that get the instance attributes, compare objects, and
+ * convert the object into a String.
+ * @author Darrel Dsouza
+ * @author Anton Derkach
+ */
 public class Patient implements Comparable<Patient> {
 	private String fname;
 	private String lname;
 	private Date dob;
-	
+
+	/**
+	 * Instantiates a Patient object with a String name and DOB given as the input.
+	 * @param name The patient's name as a String
+	 * @param dob The patient's DOB as a String
+	 */
 	public Patient(String name, String dob) {
 		String[] splitName = name.split(" ");
 		fname = splitName[0];
 		lname = splitName[1];
 		this.dob = new Date(dob);
 	}
-	
+
+	/**
+	 * Accesses the DOB
+	 * @return The DOB as a date object
+	 */
 	public Date getDOB() {
 		return dob;
 	}
+
+	/**
+	 * Converts the object into a String.
+	 * @return The String consisting of the first name, last name, and the DOB.
+	 */
 	@Override
 	public String toString() {
 		return fname + " " + lname + ", DOB: " + dob.toString();
 	}
-	
+
+	/**
+	 * Compares the object to another object. First compares the last name, then first name, then the DOB.
+	 * @param patient The other patient object to compare to.
+	 * @return The integer 1 if the instance object is greater than the other object, -1 if less, and 0 if equal.
+	 */
 	@Override
 	public int compareTo(Patient patient) {
-		if(lname.compareTo(patient.lname) < 0) {
-			return -1;
+		if(lname.compareTo(patient.lname) < Constant.EQUAL) {
+			return Constant.LESS;
 		}
-		else if(lname.compareTo(patient.lname) > 0) {
-			return 1;
+		else if(lname.compareTo(patient.lname) > Constant.EQUAL) {
+			return Constant.GREATER;
 		}
 		else {
-			if(fname.compareTo(patient.fname) < 0) {
-				return -1;
+			if(fname.compareTo(patient.fname) < Constant.EQUAL) {
+				return Constant.LESS;
 			}
-			else if (fname.compareTo(patient.fname) > 0) {
-				return 1;
+			else if (fname.compareTo(patient.fname) > Constant.EQUAL) {
+				return Constant.GREATER;
 			}
 			else {
 				return dob.compareTo(patient.dob);
 			}
 		}
 	}
-	
+
+	/**
+	 * Tests the Patient class constructor and methods.
+	 * @param args Not used. Here by default.
+	 */
 	public static void main (String[] args) {
 		Patient patient1 = new Patient("John Doe", "02/26/1966");
 		Patient patient2 = new Patient("George Smith", "2/26/2025");
